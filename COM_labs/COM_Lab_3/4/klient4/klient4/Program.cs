3d13cf7b3c19f98ff5ae0279c3568e1d449a1d4f
SbchLcj <sobiech.alicja.22@gmail.com>
2026-03-18 17:18:54 +0100

    lab com3 finished (9/10pts)

---
+++ COM_labs/COM_Lab_3/4/klient4/klient4/Program.cs
@@ -0,0 +1,30 @@
+﻿using KlasaLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class klient4
+{
+        public uint Test(string napis)
+        {
+            try
+            {
+                var obj = new Klasa();
+                obj.Test(napis);
+                return 0;
+            }
+            catch (Exception ex) 
+            { 
+                Console.WriteLine(ex.Message);
+                return 1;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            klient4 klient = new klient4();
+            Console.WriteLine(klient.Test("Testowanie, zadanie 4 ok!"));
+        }
+    }
---
+++ COM_labs/COM_Lab_3/7/zad7/zad7/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using MediaPlayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WMPLib;
+using MessageBox = System.Windows.MessageBox;
+
+namespace zad7
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //private AxAcroPDF1 axPdf;
+        //private AxWindowsMediaPlayer axPlayer;
+
+        private System.Windows.Forms.WebBrowser winFormsBrowser;
+        private System.Windows.Forms.WebBrowser pdfWebViewer;
+        private WindowsMediaPlayer vidPlayer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // strona www
+            winFormsBrowser = new System.Windows.Forms.WebBrowser();
+            winFormsBrowser.Dock = DockStyle.Fill;
+            webHost.Child = winFormsBrowser;
+            winFormsBrowser.Navigate("https://www.example.com/");
+
+            //windows media player
+            playerHost.Source = new Uri(@"C:\Users\sobal\Downloads\exampleWAV.wav"); // <-- replace with your WAV file
+            playerHost.Play();
+
+            //pdf file
+            pdfWebViewer = new System.Windows.Forms.WebBrowser();
+            pdfWebViewer.Dock = DockStyle.Fill;
+            pdfHost.Child = pdfWebViewer;
+           // pdfWebViewer.Navigate(new Uri("about:blank"));
+            pdfWebViewer.Navigate(@"C:\Users\sobal\Downloads\lab2_rh_ip_command_cheatsheet_1214_jcs_print.pdf");
+        }
+
+        private void mediaPlayer_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Wav file playing");
+        }
+
+        private void mediaPlayer_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Wav finished playing");
+        }
+    }
+}
+
